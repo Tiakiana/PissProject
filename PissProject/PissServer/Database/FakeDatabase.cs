e0bf79802fda5ce2c9d50a896d638a293dffcc46
Emil <emil.olesen@gmail.com>
2016-09-30 10:33:28 +0200

    Added IDataMapper & FakeDatabase

---
+++ PissProject/PissServer/Database/FakeDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PissServer.Database
+{
+    public class FakeDatabase : IDataMapper
+    {
+        public void Create(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PissProject/PissServer/Database/IDataMapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PissServer.Database
+{
+    interface IDataMapper
+    {
+        void Create(object obj);
+        object Read(int id);
+        void Update(int id, object obj);
+        void Delete(int id);
+    }
+}
